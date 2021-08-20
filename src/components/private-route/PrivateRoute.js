edd97f5109d8e04d52de639f71eb96fa3b017e1e
bulzaj <ernestsado94@gmail.com>
2021-08-20 23:02:07 +0200

    protect /dashboard route

--- src/components/private-route/PrivateRoute.js
+++ src/components/private-route/PrivateRoute.js
@@ -1,7 +1,18 @@
-import { Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 
-const PrivateRoute = (props) => (
-  <Route path={props.path}>{props.children}</Route>
-);
+const PrivateRoute = (props) => {
+  return (
+    <UserContext.Consumer>
+      {(user) =>
+        user ? (
+          <Route path={props.path}>{props.children}</Route>
+        ) : (
+          <Redirect to="/" />
+        )
+      }
+    </UserContext.Consumer>
+  );
+};
 
 export default PrivateRoute;
