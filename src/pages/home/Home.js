2ac780254cee2f4e1288ca6094483979ef2f3c42
bulzaj <ernestsado94@gmail.com>
2021-08-23 18:40:52 +0200

    fix content pusher incongruity

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,6 +8,8 @@ import backgroundImage1 from "../../assets/img/laniding_page_1.jpg";
 
 import Typography from "../../typography/typography";
 
+import { Form, Button, Container } from "react-bootstrap";
+
 const links = [{ to: "/dashboard", label: "Start Now!" }];
 
 const Home = () => {
@@ -27,6 +29,23 @@ const Home = () => {
             For demanding
           </Typography.Title>
         </CenterBox>
+      </Section>
+      <Section id="join" styles={["full-height", "background-grey-light"]}>
+        <Container>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Container>
       </Section>
     </div>
   );
