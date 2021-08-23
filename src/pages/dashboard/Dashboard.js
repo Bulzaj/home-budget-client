d6e87065d5cc69b258cd43a669f549b51ec7d620
bulzaj <ernestsado94@gmail.com>
2021-08-23 14:25:25 +0200

    get rid of keycloak

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,25 +1,6 @@
-import { useState, useEffect } from "react";
-import Keycloak from "keycloak-js";
 import { Alert } from "react-bootstrap";
 
 const Dashboard = (props) => {
-  const [keycloak, setKeycloak] = useState(null);
-  const [isAuth, setIsAuth] = useState(false);
-
-  useEffect(() => {
-    const keycloak = new Keycloak("/keycloak.json");
-    keycloak.init({ onLoad: "login-required" }).then((authenticated) => {
-      setKeycloak(keycloak);
-      setIsAuth(authenticated);
-    });
-  }, []);
-
-  if (keycloak) {
-    if (isAuth) {
-      return (
-        <Alert variant="success">Welcome in Home Budget app dashboard!</Alert>
-      );
-    } else return <h1>Unable to login</h1>;
-  } else return <h1>Initializing Keycloak</h1>;
+  return <Alert variant="success">Welcome in Dashboard</Alert>;
 };
 export default Dashboard;
