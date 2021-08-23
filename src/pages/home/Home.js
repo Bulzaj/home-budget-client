06e17b2458c23bad6c18fb5efb3f516d48badbf3
bulzaj <ernestsado94@gmail.com>
2021-08-23 20:53:35 +0200

    remove bootstrap

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Section from "../../components/section/Section";
 import CenterBox from "../../layouts/center-box";
-
 import BackgroundImage from "../../components/background-image/background-image";
 import backgroundImage1 from "../../assets/img/laniding_page_1.jpg";
-
+import newUserImage from "../../assets/img/new_user.png";
 import Typography from "../../typography/typography";
-
-import { Form, Button, Container } from "react-bootstrap";
+import { HashLink } from "react-router-hash-link";
 
 const Home = () => {
   return (
@@ -24,26 +22,19 @@ const Home = () => {
           <Typography.Title styles={["background-gradient-secondary"]}>
             For demanding
           </Typography.Title>
-          <Button>Start now!</Button>
+          <HashLink
+            className="link link--white link--margin-big"
+            smooth
+            to="#join"
+          >
+            Join now!
+          </HashLink>
         </CenterBox>
       </Section>
-      <Section id="join" styles={["full-height", "background-grey-light"]}>
-        <Container>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Container>
-      </Section>
+      <Section
+        id="join"
+        styles={["full-height", "background-grey-light"]}
+      ></Section>
     </div>
   );
 };
