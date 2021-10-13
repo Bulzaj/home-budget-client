646bf7021490f18bb41087fdcfe08797d4e374f2
bulzaj <ernestsado94@gmail.com>
2021-10-13 19:05:06 +0200

    redesign List component. More reusable since now

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -3,11 +3,19 @@ import { styles } from "../../utill/styles";
 
 const List = (props) => {
   if (props.items && Array.isArray(props.items)) {
+    let i = 0;
+
     const items = props.items.map((item) => {
-      return <li key={item}>{item}</li>;
+      const itemKey = item[props.itemKey] || i++;
+
+      return (
+        <li key={itemKey}>{props.wrapper ? props.wrapper(item) : item}</li>
+      );
     });
+
     return <ul className={styles(classes, "list", props.styles)}>{items}</ul>;
   }
+  throw new Error("No items provided in props object");
 };
 
 export default List;
