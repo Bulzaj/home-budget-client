08bdb6257423bee2f988ef2c8584d10b1dabff3a
bulzaj <ernestsado94@gmail.com>
2021-10-26 16:14:09 +0200

    refactor accounts hook

--- src/hooks/use-accounts.js
+++ src/hooks/use-accounts.js
@@ -3,6 +3,7 @@ import { useAuth } from "./use-auth";
 import { useHistory } from "react-router";
 import axios from "axios";
 import { URL_API_ACCOUNT } from "../utill/url-consts";
+import useFetch from "./use-fetch";
 
 const accountsContext = createContext();
 
@@ -23,20 +24,12 @@ const useProvideContext = () => {
   const [selectedAccount, setSelectedAccount] = useState(null);
   const accessToken = useAuth().getAccessToken();
   const redirect = useHistory().push;
+  const fetch = useFetch(accessToken);
 
   useEffect(() => {
-    const config = {
-      headers: {
-        authorization: `bearer ${accessToken}`,
-      },
-    };
     const fetchAccounts = async () => {
-      try {
-        const accounts = await axios.get(URL_API_ACCOUNT, config);
-        setAccounts(accounts.data);
-      } catch (err) {
-        redirect("/");
-      }
+      const result = await fetch(URL_API_ACCOUNT);
+      setAccounts(result.data);
     };
     fetchAccounts();
   }, [accessToken, redirect]);
