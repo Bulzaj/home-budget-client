986b730ee49b99adf5c5ce97a7de436251d28ec8
bulzaj <ernestsado94@gmail.com>
2021-10-28 13:41:14 +0200

    create registration form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,21 @@
-import LoginForm from "../../containers/login-form/login-form";
+import { useState } from "react";
+import LoginForm from "../../containers/auth-forms/login-form";
+import RegistrationForm from "../../containers/auth-forms/registration-form";
 import classes from "./home.module.css";
 
 const Home = () => {
-  return (
-    <div className={classes.home}>
-      <LoginForm />
-    </div>
-  );
+  const [isLogin, setIsLogin] = useState(true);
+
+  const onToggleFormHandler = (e) => {
+    e.preventDefault();
+    setIsLogin((prevState) => !prevState);
+  };
+
+  let selectedForm = <LoginForm onToggleFormClick={onToggleFormHandler} />;
+  if (!isLogin)
+    selectedForm = <RegistrationForm onToggleFormClick={onToggleFormHandler} />;
+
+  return <div className={classes.home}>{selectedForm}</div>;
 };
 
 export default Home;
