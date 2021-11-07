90e3b39afc71d8d4d4e27507cb0853b2e2d51dd3
bulzaj <ernestsado94@gmail.com>
2021-11-07 20:58:24 +0100

    refactor specification data privider hook. Move fetching data directly to components

--- src/hooks/use-spec.js
+++ src/hooks/use-spec.js
@@ -1,12 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import {
-  URL_API_SPEC_CASH_FLOW,
-  URL_API_SPEC_EXPENDITURES,
-} from "../utill/url-consts";
-import { useAccounts } from "./use-accounts";
-import { useAuth } from "./use-auth";
-import useFetch from "./use-fetch";
-import { useFilters } from "./use-filter";
+import { createContext, useContext, useState } from "react";
 
 const specContext = createContext();
 
@@ -21,79 +13,16 @@ export const ProvideSpec = (props) => {
 export const useSpec = () => useContext(specContext);
 
 const useProvideSpec = () => {
-  const accessToken = useAuth().getAccessToken();
-  const { selectedAccount } = useAccounts();
-  const { dateFilter } = useFilters();
-
-  const expendituresSpec = useFetchExpenditures(
-    accessToken,
-    selectedAccount,
-    dateFilter
-  );
-  const cashFlow = useFetchCashFlow(accessToken, selectedAccount, dateFilter);
-
-  return {
-    expendituresSpec: expendituresSpec.expendituresSpec,
-    fetchExpendituresSpec: expendituresSpec.fetchExpenduturesSpec,
-    cashFlow: cashFlow.cashFlow,
-    fetchCashFlow: cashFlow.fetchCashFlow,
-  };
-};
-
-const useFetchExpenditures = (accessToken, selectedAccount, dateFilter) => {
+  const [operationsHistory, setOperationsHistory] = useState(null);
   const [expendituresSpec, setExpendituresSpec] = useState(null);
-  const fetch = useFetch(accessToken);
-
-  const fetchExpenduturesSpec = async () => {
-    if (!selectedAccount) return;
-    const url = `${URL_API_SPEC_EXPENDITURES}/${selectedAccount._id}`;
-    const queries = {
-      from: dateFilter?.from,
-      to: dateFilter?.to,
-    };
-
-    const result = await fetch(url, queries);
-
-    setExpendituresSpec(result.data);
-  };
-
-  useEffect(() => {
-    fetchExpenduturesSpec();
-  }, [accessToken, selectedAccount]);
+  const [cashFlow, setCashFlow] = useState(null);
 
   return {
+    operationsHistory,
+    setOperationsHistory,
     expendituresSpec,
-    fetchExpenduturesSpec,
-  };
-};
-
-const useFetchCashFlow = (accessToken, selectedAccount, dateFilter) => {
-  const [cashFlow, setCashFlow] = useState([]);
-  const fetch = useFetch(accessToken);
-
-  const fetchCashFlow = async () => {
-    if (!selectedAccount) return;
-
-    const url = `${URL_API_SPEC_CASH_FLOW}/${selectedAccount._id}`;
-    const queries = {
-      from: dateFilter?.from,
-      to: dateFilter?.to,
-    };
-
-    try {
-      const result = await fetch(url, queries);
-      setCashFlow(result.data);
-    } catch (err) {
-      throw err;
-    }
-  };
-
-  useEffect(() => {
-    fetchCashFlow();
-  }, [accessToken, selectedAccount]);
-
-  return {
+    setExpendituresSpec,
     cashFlow,
-    fetchCashFlow,
+    setCashFlow,
   };
 };
